189f65207a95a45faaf9f4a28e54cf5a597b6762
yongpeishi <yongpeishi@gmail.com>
2019-06-30 16:32:44 +1000

    hardcode a world

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { List, Set } from 'immutable'
+
 import './App.css';
 import World from './game-of-life/World'
 
+
 function App() {
+  const aliveCells = Set([
+    List([0,0]),
+    List([1,2]),
+    List([2,2])
+  ])
+
   return (
     <div className="App">
-      <World></World>
+      <World rows={3} columns={3} aliveCells={aliveCells}></World>
     </div>
   );
 }
--- src/game-of-life/World.js
+++ src/game-of-life/World.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import { List, Range } from 'immutable'
 
 import Cell from './Cell'
+import './World.css'
 
-function World(props) {
+const World = ({rows, columns, aliveCells}) => {
+  const cellsStatus = (aliveCells, rows, columns) => {
+    return Range(0, rows).flatMap(r => {
+      return Range(0, columns).map(c => {
+        return aliveCells.includes(List([r, c])) ? true : false
+      })
+    })
+  }
+
   return (
-    <div>
-      <h1>Hello cat </h1>
-      <Cell isAlive={true}></Cell>
-      <Cell isAlive={false}></Cell>
-      <h1>end</h1>
+    <div className='world'>
+      {
+        cellsStatus(aliveCells, rows, columns).map( (status, index) => {
+          return <Cell isAlive={status} key={index}></Cell>
+        })
+      }
     </div>
   )
 }
